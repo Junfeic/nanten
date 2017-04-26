e5bb6944354cb7c1452d75ec07346ad78f54296c
junhong <chenjunfei@gogbuy.com>
2017-04-26 17:17:54 +0800

    This demo illustrates how stapler staple urls to jelly page or object method.

---
+++ stapler-jelly-demo/src/main/java/com/gogbuy/stapler/demo/BookStore.java
@@ -0,0 +1,79 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2017-2020, Nanten Cloud LLC. Junhong Chen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.gogbuy.stapler.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.gogbuy.stapler.demo.item.Book;
+import com.gogbuy.stapler.demo.item.CD;
+import com.gogbuy.stapler.demo.item.Item;
+
+/**
+ * The root object serving the root URL /
+ * @author junho.chen@outlook.com
+ *
+ */
+public class BookStore {
+
+	/**
+	 * Stores various objects scoped to {@link BookStore}
+	 */
+	public static BookStore theStore = new BookStore();
+
+	public String getMyString() {
+		return "Hello, Stapler Jelly!";
+	}
+
+	public String getNoJelly() {
+		return "getNoJelly";
+	}
+
+	public String doNoJelly() {
+		return "There's no jelly associated with this doNoJelly() method";
+	}
+
+	public Map<String, Item> getItems() {
+		Map<String, Item> items = new HashMap<String, Item>();
+		items.put("book", new Book("Brave heart"));
+		items.put("cd", new CD("Mozart"));
+		return items;
+	}
+
+	/**
+	 * Polymorphism and parameter test.
+	 * @param name
+	 * @return
+	 */
+	public Item getItem(String name) {
+		if(name == null || name.equals("")) {
+			return new Book("The Chaos Classics");
+		}
+		if(name.startsWith("b")) {
+			return new Book(name);
+		}
+		return new CD(name);
+	}
+}
